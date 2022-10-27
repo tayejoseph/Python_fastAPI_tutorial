955d4af33c18071e91a037ad7b6f83754e70440f
Taye Joseph <tayejoseph6@gmail.com>
2022-10-27 02:11:17 +0100

    restructured code

--- app/main.py
+++ app/main.py
@@ -1,19 +1,15 @@
 from os import curdir
 from turtle import title
-from typing import Optional, List
+from typing import List
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
-from random import randrange
-from passlib.context import CryptContext
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
-from . import models, schemas
+from . import models, schemas, utils
 from sqlalchemy.orm import Session
 from .database import engine, get_db
-
-# hashing algorithm
-pwd_context = CryptContext(schemes=["bcrypt"], depreciated='auto')
+from .routers import post, user
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -45,96 +41,10 @@ def find_index_post(id: int):
         if p["id"] == id:
             return i
 
+
+app.include_router(post.router)
+app.include_router(user.router)
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
-
-@app.get("/posts", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)): 
-    # with regular sql
-    # cursor.execute("""SELECT * FROM posts""")
-    # posts = cursor.fetchall()   
-    posts = db.query(models.Post).all()
-    return posts
-
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-# this validate the payload based on the pydentic validator
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):   
-    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-    # conn.commit()
-
-    # new_post = models.Post(title = post.title, content=post.content, published=post.published)
-    # so instead of distructure post one after the other you can just do the following bellow **post.dict()
-    new_post = models.Post(**post.dict())
-    db.add(new_post)
-    db.commit()
-    # this returns back the post
-    db.refresh(new_post)
-    return new_post
-    
-
-@app.get("/posts/{id}", response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db)):    
-    # cursor.execute("""SELECT * from posts WHERE id = %s """, (str(id)))
-    # post = cursor.fetchone()
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-    
-    if  not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {"message": f"post with id: {id} was not found"}
-    return post
-
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
-
-    # cursor.execute("""DELETE FROM posts WHERE id = %s returning *""", (str(id)))
-    # delete_post = cursor.fetchone()
-
-    # conn.commit()
-    post = db.query(models.Post).filter(models.Post.id == id)
-
-    
-    if post.first() == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
-
-    # this deletes the post if it exists
-    post.delete(synchronize_session=False)
-    db.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.put("/posts/{id}", response_model=schemas.Post)
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
-
-    # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s  RETURNING *""", (post.title, post.content, post.published, str(id)))
-    # conn.commit()
-    # updated_post = cursor.fetchone()
-
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-
-    post = post_query.first()
-
-    if post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
-
-    post_query.update(updated_post.dict(),synchronize_session=False)
-    db.commit()
-
-    return post_query.first()
-
-
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    # hash the password
-    hashed_password = pwd_context.hash(user.password)
-    user.password = hashed_password
-    
-    new_user = models.User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    # this returns back the post
-    db.refresh(new_user)
-    return new_user
