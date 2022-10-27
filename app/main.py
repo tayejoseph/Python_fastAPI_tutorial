fea4bb0c8a09aa3e521df99236b1a4eab3a0b9ef
Taye Joseph <tayejoseph6@gmail.com>
2022-10-27 01:30:07 +0100

    hashed password

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from typing import Optional, List
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from random import randrange
+from passlib.context import CryptContext
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
@@ -11,7 +12,8 @@ from . import models, schemas
 from sqlalchemy.orm import Session
 from .database import engine, get_db
 
-
+# hashing algorithm
+pwd_context = CryptContext(schemes=["bcrypt"], depreciated='auto')
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -126,6 +128,10 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
 
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    # hash the password
+    hashed_password = pwd_context.hash(user.password)
+    user.password = hashed_password
+    
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
